f6c0bc6b53f5093455b71dfd6a9779fd3a763f2c
Jan Hoffmann <jan@3e8.eu>
2021-07-23 00:29:57 +0200

    Add support for bezier curves in SVG paths

--- graphs/path.go
+++ graphs/path.go
@@ -18,6 +18,8 @@ type path struct {
 	openY       float64
 	lastX       float64
 	lastY       float64
+	lastX2      float64
+	lastY2      float64
 }
 
 func (p *path) SetPrecision(digits uint) {
@@ -54,11 +56,18 @@ func (p *path) formatCoords(x, y float64) (xStr, yStr string) {
 	return
 }
 
-func (p *path) buildCommand(cmd, xStr, yStr string) string {
-	if yStr[0] == '-' || (yStr[0] == '.' && strings.Contains(xStr, ".")) {
-		return cmd + xStr + yStr
+func (p *path) buildCommand(cmd string, coords ...string) string {
+	out := cmd
+	var lastC string
+	for i, c := range coords {
+		if i == 0 || c[0] == '-' || (c[0] == '.' && strings.Contains(lastC, ".")) {
+			out += c
+		} else {
+			out += " " + c
+		}
+		lastC = c
 	}
-	return cmd + xStr + " " + yStr
+	return out
 }
 
 func (p *path) MoveTo(x, y float64) {
@@ -79,6 +88,8 @@ func (p *path) MoveTo(x, y float64) {
 	p.openY = y
 	p.lastX = x
 	p.lastY = y
+	p.lastX2 = x
+	p.lastY2 = y
 }
 
 func (p *path) LineTo(x, y float64) {
@@ -113,6 +124,49 @@ func (p *path) LineTo(x, y float64) {
 
 	p.lastX = x
 	p.lastY = y
+	p.lastX2 = x
+	p.lastY2 = y
+}
+
+func (p *path) BezierCurveTo(x1, y1, x2, y2, x, y float64) {
+	x1, y1 = p.roundCoords(x1, y1)
+	x1Str, y1Str := p.formatCoords(x1, y1)
+	x2, y2 = p.roundCoords(x2, y2)
+	x2Str, y2Str := p.formatCoords(x2, y2)
+	x, y = p.roundCoords(x, y)
+	xStr, yStr := p.formatCoords(x, y)
+	cmd := p.buildCommand("C", x1Str, y1Str, x2Str, y2Str, xStr, yStr) // absolute curveto
+
+	smoothCheckLastX2, smoothCheckLastY2 := p.formatCoords(p.lastX-p.lastX2, p.lastY-p.lastY2)
+	smoothCheckX1, smoothCheckY1 := p.formatCoords(x1-p.lastX, y1-p.lastY)
+
+	dx1, dy1 := p.roundCoords(x1-p.lastX, y1-p.lastY)
+	dx1Str, dy1Str := p.formatCoords(dx1, dy1)
+	dx2, dy2 := p.roundCoords(x2-p.lastX, y2-p.lastY)
+	dx2Str, dy2Str := p.formatCoords(dx2, dy2)
+	dx, dy := p.roundCoords(x-p.lastX, y-p.lastY)
+	dxStr, dyStr := p.formatCoords(dx, dy)
+	if dxStr == "0" && dyStr == "0" {
+		return
+	} else if smoothCheckLastX2 == smoothCheckX1 && smoothCheckLastY2 == smoothCheckY1 {
+		cmd = p.buildCommand("S", x2Str, y2Str, xStr, yStr)              // absolute smooth curveto
+		cmdRelative := p.buildCommand("s", dx2Str, dy2Str, dxStr, dyStr) // relative smooth curveto
+		if len(cmdRelative) < len(cmd) {
+			cmd = cmdRelative
+		}
+	} else {
+		cmdRelative := p.buildCommand("c", dx1Str, dy1Str, dx2Str, dy2Str, dxStr, dyStr) // relative curveto
+		if len(cmdRelative) < len(cmd) {
+			cmd = cmdRelative
+		}
+	}
+
+	fmt.Fprint(&p.sb, cmd)
+
+	p.lastX = x
+	p.lastY = y
+	p.lastX2 = x2
+	p.lastY2 = y2
 }
 
 func (p *path) AddPath(otherPath path) {
@@ -122,6 +176,8 @@ func (p *path) AddPath(otherPath path) {
 	p.openY = otherPath.openY
 	p.lastX = otherPath.lastX
 	p.lastY = otherPath.lastY
+	p.lastX2 = otherPath.lastX2
+	p.lastY2 = otherPath.lastY2
 }
 
 func (p *path) Close() {
