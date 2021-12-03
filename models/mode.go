janh/go-dsl

commit 8626b510b87e8836267dc6497b43a3b0f63d542c

Add initial support for MediaTek modems
For now, only the subset of information which is easily available from
procfs is supported.

There are other ways to get more detailed information, such as the
"adslphxcmd" and "wan" commands. However, the former doesn't work on the
tested device (the kernel log shows "(get_adsl_dev_ops) p is NULL").
And the output of the "wan" ("tcci") command is printed to the kernel
log instead of stdout, making it difficult to parse (especially for long
output, such as bitloading/SNR/QLN/Hlog).

models/mode.go
@@ -6,6 +6,7 @@ package models
 
 import (
 	"strings"
+	"unicode"
 )
 
 type ModeType int
@@ -159,13 +160,18 @@ func (m Mode) CarrierSpacing() float64 {
 }
 
 func ParseMode(str string) Mode {
-	str = strings.ToLower(strings.TrimSpace(str))
+	str = strings.Map(func(r rune) rune {
+		if unicode.IsSpace(r) || r == '_' {
+			return -1
+		}
+		return unicode.ToLower(r)
+	}, str)
 
 	var mode Mode
 
 	switch {
 
-	case strings.Contains(str, "adsl"):
+	case strings.Contains(str, "adsl"), strings.Contains(str, "g.dmt"), strings.Contains(str, "g.992"):
 		if strings.Contains(str, "adsl2+") || strings.Contains(str, "adsl2p") {
 			mode.Type = ModeTypeADSL2Plus
 		} else if strings.Contains(str, "adsl2") {
@@ -232,7 +238,7 @@ func ParseMode(str string) Mode {
 		mode.Type = ModeTypeVDSL2
 		mode.Subtype = ModeSubtypeProfile35b
 
-	case strings.Contains(str, "vdsl2"):
+	case strings.Contains(str, "vdsl2"), strings.Contains(str, "g.993.2"), strings.Contains(str, "g.993.5"):
 		mode.Type = ModeTypeVDSL2
 
 	}

cmd/main.go
@@ -27,6 +27,7 @@ import (
 	_ "3e8.eu/go/dsl/draytek"
 	_ "3e8.eu/go/dsl/fritzbox"
 	_ "3e8.eu/go/dsl/lantiq"
+	_ "3e8.eu/go/dsl/mediatek"
 )
 
 var (

mediatek/bins.go
@@ -1,0 +1,92 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package mediatek
+
+import (
+	"strconv"
+	"strings"
+
+	"3e8.eu/go/dsl/internal/helpers"
+	"3e8.eu/go/dsl/models"
+)
+
+func parseBins(status models.Status, adslShowbpcDs, adslShowbpcUs, adslShowsnr, vdslShowbpcDs, vdslShowbpcUs, vdslShowsnr string) models.Bins {
+	var bins models.Bins
+
+	bins.Mode = status.Mode
+
+	var bitsDS, bitsUS, snr string
+	if bins.Mode.Type == models.ModeTypeVDSL2 {
+		bitsDS = vdslShowbpcDs
+		bitsUS = vdslShowbpcUs
+		snr = vdslShowsnr
+	} else {
+		bitsDS = adslShowbpcDs
+		bitsUS = adslShowbpcUs
+		snr = adslShowsnr
+	}
+
+	parseBits(&bins.Bits.Downstream, bitsDS)
+	parseBits(&bins.Bits.Upstream, bitsUS)
+
+	helpers.GenerateBandsData(&bins)
+
+	parseSNR(&bins, snr)
+
+	return bins
+}
+
+func parseBinList(str string) []string {
+	items := strings.Split(str, ",")
+
+	for i, item := range items {
+		if len(item) >= 2 && item[0] == '"' && item[len(item)-1] == '"' {
+			item = item[1 : len(item)-1]
+		}
+
+		items[i] = strings.TrimSpace(item)
+	}
+
+	return items
+}
+
+func parseBits(out *models.BinsBits, data string) {
+	values := parseBinList(data)
+
+	out.Data = make([]int8, len(values))
+
+	for num, val := range values {
+		valInt, err := strconv.ParseInt(val, 10, 8)
+		if err == nil && valInt > 0 {
+			out.Data[num] = int8(valInt)
+		}
+	}
+}
+
+func parseSNR(bins *models.Bins, data string) {
+	values := parseBinList(data)
+
+	bins.SNR.Downstream.GroupSize = 1
+	bins.SNR.Downstream.Data = make([]float64, len(values))
+
+	bins.SNR.Upstream.GroupSize = 1
+	bins.SNR.Upstream.Data = make([]float64, len(values))
+
+	bandDecider, err := helpers.NewBandDecider(bins.Bands)
+	if err != nil {
+		return
+	}
+
+	for num, val := range values {
+		valFloat, err := strconv.ParseFloat(val, 64)
+		if err == nil && valFloat > 0 {
+			if bandDecider.IsDownstream(num) {
+				bins.SNR.Downstream.Data[num] = float64(valFloat)
+			} else {
+				bins.SNR.Downstream.Data[num] = float64(valFloat)
+			}
+		}
+	}
+}

mediatek/config.go
@@ -1,0 +1,23 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package mediatek
+
+import (
+	"3e8.eu/go/dsl"
+)
+
+type TelnetConfig struct {
+	Host     string
+	User     string
+	Password dsl.PasswordCallback
+}
+
+type SSHConfig struct {
+	Host        string
+	User        string
+	Password    dsl.PasswordCallback
+	PrivateKeys dsl.PrivateKeysCallback
+	KnownHosts  string
+}

mediatek/register.go
@@ -1,0 +1,42 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package mediatek
+
+import (
+	"3e8.eu/go/dsl"
+)
+
+func init() {
+	newTelnet := func(config dsl.Config) (dsl.Client, error) {
+		telnetConfig := TelnetConfig{
+			Host:     config.Host,
+			User:     config.User,
+			Password: config.AuthPassword,
+		}
+		return NewTelnetClient(telnetConfig)
+	}
+	clientDescTelnet := dsl.ClientDesc{
+		RequiresUser:       dsl.TristateMaybe,
+		SupportedAuthTypes: dsl.AuthTypePassword,
+	}
+	dsl.RegisterClient("mediatek_telnet", newTelnet, clientDescTelnet)
+
+	newSSH := func(config dsl.Config) (dsl.Client, error) {
+		sshConfig := SSHConfig{
+			Host:        config.Host,
+			User:        config.User,
+			Password:    config.AuthPassword,
+			PrivateKeys: config.AuthPrivateKeys,
+			KnownHosts:  config.KnownHosts,
+		}
+		return NewSSHClient(sshConfig)
+	}
+	clientDescSSH := dsl.ClientDesc{
+		RequiresUser:       dsl.TristateYes,
+		SupportedAuthTypes: dsl.AuthTypePassword | dsl.AuthTypePrivateKeys,
+		RequiresKnownHosts: true,
+	}
+	dsl.RegisterClient("mediatek_ssh", newSSH, clientDescSSH)
+}

mediatek/ssh.go
@@ -1,0 +1,51 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package mediatek
+
+import (
+	"3e8.eu/go/dsl/internal/ssh"
+	"3e8.eu/go/dsl/models"
+)
+
+type SSHClient struct {
+	client  *ssh.Client
+	rawData []byte
+	status  models.Status
+	bins    models.Bins
+}
+
+func NewSSHClient(config SSHConfig) (*SSHClient, error) {
+	c := SSHClient{}
+
+	var err error
+
+	c.client, err = ssh.NewClient(config.Host, config.User, config.Password, config.PrivateKeys, config.KnownHosts)
+	if err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+}
+
+func (c *SSHClient) RawData() []byte {
+	return c.rawData
+}
+
+func (c *SSHClient) Status() models.Status {
+	return c.status
+}
+
+func (c *SSHClient) Bins() models.Bins {
+	return c.bins
+}
+
+func (c *SSHClient) UpdateData() (err error) {
+	c.status, c.bins, c.rawData, err = updateData(c.client)
+	return
+}
+
+func (c *SSHClient) Close() {
+	c.client.Close()
+}

mediatek/status.go
@@ -1,0 +1,254 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package mediatek
+
+import (
+	"bufio"
+	"math"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"3e8.eu/go/dsl/internal/helpers"
+	"3e8.eu/go/dsl/models"
+)
+
+var regexpFilterCharacters = regexp.MustCompile(`[^a-zA-Z0-9]+`)
+var regexpNumber = regexp.MustCompile("[0-9]+")
+
+func parseStatus(adslStats, vdslInterfaceConfig, adslFwVer string) models.Status {
+	var status models.Status
+
+	values := parseKeyValue(adslStats, vdslInterfaceConfig)
+
+	parseStatusBasic(&status, values)
+
+	parseStatusRates(&status, values)
+	parseStatusLine(&status, values)
+
+	parseStatusINP(&status, values)
+
+	parseStatusVectoring(&status, values)
+
+	parseStatusErrors(&status, values)
+
+	parseFarVersion(&status, values)
+	parseFirmwareVersion(&status, adslFwVer)
+
+	return status
+}
+
+func parseKeyValue(data ...string) map[string]string {
+	values := make(map[string]string)
+
+	for _, item := range data {
+		scanner := bufio.NewScanner(strings.NewReader(item))
+
+		for scanner.Scan() {
+			line := scanner.Text()
+			split := strings.SplitN(line, ":", 2)
+
+			if len(split) == 2 {
+				key := strings.ToLower(regexpFilterCharacters.ReplaceAllString(split[0], ""))
+				val := strings.TrimSpace(split[1])
+
+				values[key] = val
+			}
+		}
+	}
+
+	return values
+}
+
+func interpretString(values map[string]string, key string) string {
+	if val, ok := values[key]; ok {
+		return val
+	}
+	return ""
+}
+
+func interpretDuration(values map[string]string, key string) models.Duration {
+	var duration models.Duration
+
+	if val, ok := values[key]; ok {
+		numbers := regexpNumber.FindAllString(val, -1)
+
+		for i, num := range numbers {
+			count := len(numbers) - i
+			numInt, _ := strconv.ParseInt(num, 10, 64)
+
+			var factor time.Duration
+			if count == 1 {
+				factor = time.Second
+			} else if count == 2 {
+				factor = time.Minute
+			} else if count == 3 {
+				factor = time.Hour
+			} else if count == 4 {
+				factor = 24 * time.Hour
+			}
+
+			duration.Duration += time.Duration(numInt) * factor
+		}
+	}
+
+	return duration
+}
+
+func interpretIntValue(values map[string]string, suffix string, keys ...string) (out models.IntValue) {
+	for _, key := range keys {
+		if val, ok := values[key]; ok {
+			if suffix != "" && strings.HasSuffix(val, suffix) {
+				val = val[:len(val)-len(suffix)]
+			}
+
+			if valInt, err := strconv.ParseInt(val, 10, 64); err == nil {
+				out.Int = valInt
+				out.Valid = true
+
+				if valInt != 0 {
+					return
+				}
+			}
+		}
+	}
+
+	return
+}
+
+func interpretFloatValue(values map[string]string, suffix string, keys ...string) (out models.FloatValue) {
+	for _, key := range keys {
+		if val, ok := values[key]; ok {
+			if suffix != "" && strings.HasSuffix(val, suffix) {
+				val = val[:len(val)-len(suffix)]
+			}
+
+			if strings.HasPrefix(val, "0x") {
+				if valUint, err := strconv.ParseUint(val[2:], 16, 32); err == nil {
+					out.Float = float64(math.Float32frombits(uint32(valUint)))
+					out.Valid = true
+				}
+			} else {
+				if valFloat, err := strconv.ParseFloat(val, 64); err == nil {
+					out.Float = valFloat
+					out.Valid = true
+				}
+			}
+
+			if out.Valid && out.Float != 0 {
+				break
+			}
+		}
+	}
+
+	return
+}
+
+func parseStatusBasic(status *models.Status, values map[string]string) {
+
+	status.State = models.ParseState(interpretString(values, "adsllinkstatus"))
+
+	opmode := interpretString(values, "opmode")
+	annex := interpretString(values, "adsltype")
+	profile := interpretString(values, "currentprofiles")
+	status.Mode = models.ParseMode(opmode + " " + annex + " " + profile)
+
+	status.Uptime = interpretDuration(values, "adsluptime")
+}
+
+func parseStatusRates(status *models.Status, values map[string]string) {
+
+	status.DownstreamActualRate.IntValue = interpretIntValue(values, " kbps",
+		"nearendinterleavedchannelbitrate", "nearendfastchannelbitrate")
+	status.UpstreamActualRate.IntValue = interpretIntValue(values, " kbps",
+		"farendinterleavedchannelbitrate", "farendfastchannelbitrate")
+
+	status.DownstreamAttainableRate.IntValue = interpretIntValue(values, " kbps", "attaindownstream")
+	status.UpstreamAttainableRate.IntValue = interpretIntValue(values, " kbps", "attainupstream")
+}
+
+func parseStatusLine(status *models.Status, values map[string]string) {
+
+	status.DownstreamAttenuation.FloatValue = interpretFloatValue(values, " dB", "attenuationdownstream")
+	status.UpstreamAttenuation.FloatValue = interpretFloatValue(values, " dB", "attenuationupstream")
+
+	status.DownstreamSNRMargin.FloatValue = interpretFloatValue(values, " dB", "noisemargindownstream")
+	status.UpstreamSNRMargin.FloatValue = interpretFloatValue(values, " dB", "noisemarginupstream")
+
+	status.DownstreamPower.FloatValue = interpretFloatValue(values, " dbm", "outputpowerdownstream")
+	status.UpstreamPower.FloatValue = interpretFloatValue(values, " dbm", "outputpowerupstream")
+}
+
+func parseStatusINP(status *models.Status, values map[string]string) {
+
+	interleavingDepthDown := interpretIntValue(values, "", "interleavedepthdownstream")
+	if interleavingDepthDown.Valid && interleavingDepthDown.Int == 1 {
+		status.DownstreamInterleavingDelay.FloatValue.Float = 0
+		status.DownstreamInterleavingDelay.FloatValue.Valid = true
+	}
+	interleavingDepthUp := interpretIntValue(values, "", "interleavedepthupstream")
+	if interleavingDepthUp.Valid && interleavingDepthUp.Int == 1 {
+		status.UpstreamInterleavingDelay.FloatValue.Float = 0
+		status.UpstreamInterleavingDelay.FloatValue.Valid = true
+	}
+
+	status.DownstreamImpulseNoiseProtection.FloatValue = interpretFloatValue(values, "", "inpdsnormal")
+	status.UpstreamImpulseNoiseProtection.FloatValue = interpretFloatValue(values, "", "inpusnormal")
+
+	opmode := interpretString(values, "opmode")
+	opmode = strings.ToLower(regexpFilterCharacters.ReplaceAllString(opmode, ""))
+	status.DownstreamRetransmissionEnabled.Bool = strings.Contains(opmode, "ginp") && !strings.Contains(opmode, "ginpu")
+	status.DownstreamRetransmissionEnabled.Valid = true
+	status.UpstreamRetransmissionEnabled.Bool = strings.Contains(opmode, "ginp") && !strings.Contains(opmode, "ginpd")
+	status.UpstreamRetransmissionEnabled.Valid = true
+}
+
+func parseStatusVectoring(status *models.Status, values map[string]string) {
+
+	opmode := interpretString(values, "opmode")
+	opmode = strings.ToLower(regexpFilterCharacters.ReplaceAllString(opmode, ""))
+	if strings.Contains(opmode, "vector") {
+		status.DownstreamVectoringState.State = models.VectoringStateFull
+		status.DownstreamVectoringState.Valid = true
+	}
+}
+
+func parseStatusErrors(status *models.Status, values map[string]string) {
+
+	status.DownstreamFECCount = interpretIntValue(values, "", "nearendfecerrorfast", "nearendfecerrorinterleaved")
+	status.UpstreamFECCount = interpretIntValue(values, "", "farendfecerrorfast", "farendfecerrorinterleaved")
+
+	status.DownstreamCRCCount = interpretIntValue(values, "", "nearendcrcerrorfast", "nearendcrcerrorinterleaved")
+	status.UpstreamCRCCount = interpretIntValue(values, "", "farendcrcerrorfast", "farendcrcerrorinterleaved")
+}
+
+func parseFarVersion(status *models.Status, values map[string]string) {
+	data := interpretString(values, "farendituidentification")
+
+	if len(data) == 16 {
+		vendorByte := helpers.ParseHexadecimal(data[4:12])
+		status.FarEndInventory.Vendor = helpers.FormatVendor(string(vendorByte))
+
+		versionByte := helpers.ParseHexadecimal(data[12:16])
+		status.FarEndInventory.Version = helpers.FormatVersion(status.FarEndInventory.Vendor, versionByte)
+	}
+}
+
+func parseFirmwareVersion(status *models.Status, fwVer string) {
+	status.NearEndInventory.Vendor = "TrendChip"
+
+	startIndex := strings.Index(fwVer, "FwVer:")
+	if startIndex != -1 {
+		fwVer = fwVer[startIndex+6:]
+
+		endIndex := strings.IndexByte(fwVer, '_')
+		if endIndex != -1 {
+			fwVer = fwVer[:endIndex]
+		}
+
+		status.NearEndInventory.Version = fwVer
+	}
+}

mediatek/telnet.go
@@ -1,0 +1,61 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package mediatek
+
+import (
+	"3e8.eu/go/dsl/internal/telnet"
+	"3e8.eu/go/dsl/models"
+)
+
+type TelnetClient struct {
+	client  *telnet.Client
+	rawData []byte
+	status  models.Status
+	bins    models.Bins
+}
+
+func NewTelnetClient(config TelnetConfig) (*TelnetClient, error) {
+	c := TelnetClient{}
+
+	var err error
+
+	user := config.User
+	if user == "" {
+		user = "admin"
+	}
+
+	clientConfig := telnet.ClientConfig{
+		PromptAccount:  "login:",
+		PromptPassword: "Password:",
+		PromptCommand:  "# ",
+	}
+	c.client, err = telnet.NewClient(clientConfig, config.Host, user, config.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+}
+
+func (c *TelnetClient) RawData() []byte {
+	return c.rawData
+}
+
+func (c *TelnetClient) Status() models.Status {
+	return c.status
+}
+
+func (c *TelnetClient) Bins() models.Bins {
+	return c.bins
+}
+
+func (c *TelnetClient) UpdateData() (err error) {
+	c.status, c.bins, c.rawData, err = updateData(c.client)
+	return
+}
+
+func (c *TelnetClient) Close() {
+	c.client.Close()
+}

commit daa633b61c6f34aec56fc0251f8de3bd131dbd45

Fix ADSL Annex parsing
This was broken since commit 8626b510b87e.

models/mode.go
@@ -182,22 +182,22 @@ func ParseMode(str string) Mode {
 
 		switch {
 
-		case strings.Contains(str, "annex a"):
+		case strings.Contains(str, "annexa"):
 			mode.Subtype = ModeSubtypeAnnexA
 
-		case strings.Contains(str, "annex b"):
+		case strings.Contains(str, "annexb"):
 			mode.Subtype = ModeSubtypeAnnexB
 
-		case strings.Contains(str, "annex i"):
+		case strings.Contains(str, "annexi"):
 			mode.Subtype = ModeSubtypeAnnexI
 
-		case strings.Contains(str, "annex j"):
+		case strings.Contains(str, "annexj"):
 			mode.Subtype = ModeSubtypeAnnexJ
 
-		case strings.Contains(str, "annex l"):
+		case strings.Contains(str, "annexl"):
 			mode.Subtype = ModeSubtypeAnnexL
 
-		case strings.Contains(str, "annex m"):
+		case strings.Contains(str, "annexm"):
 			mode.Subtype = ModeSubtypeAnnexM
 
 		}