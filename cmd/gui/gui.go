2d2c4842470a3c62656c2dd88a05167b772df4e3
Jan Hoffmann <jan@3e8.eu>
2022-04-04 23:45:20 +0200

    Make graphical user interface independent from web server
    This makes use of the web interface components that were moved to the
    new common package.
    
    The previously missing save button is now included, with the limitation
    of always saving to the current working directory.

--- cmd/gui/gui.go
+++ cmd/gui/gui.go
@@ -7,31 +7,41 @@
 package gui
 
 import (
+	"bytes"
+	"embed"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"html/template"
+	"io/fs"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"syscall"
 	"time"
 
 	"github.com/webview/webview"
 
 	"3e8.eu/go/dsl"
+	jsgraphs "3e8.eu/go/dsl/graphs/javascript"
 
-	"3e8.eu/go/dsl/cmd/web"
+	"3e8.eu/go/dsl/cmd/web/common"
 )
 
 const Enabled = true
 
-var w webview.WebView
+//go:embed res
+var resources embed.FS
+
+var (
+	c    *common.Client
+	w    webview.WebView
+	stop chan bool
+)
 
 func Run(config dsl.Config) {
-	addr, err := web.Start(config)
-	if err != nil {
-		fmt.Println("failed to start web server:", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("Started web server at: %s\n", addr)
+	c = common.NewClient(config)
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
@@ -41,32 +51,151 @@ func Run(config dsl.Config) {
 		stopWebView()
 	}()
 
-	startWebView(addr)
+	startWebView()
 
 	go func() {
 		<-time.After(30 * time.Second)
 		os.Exit(1)
 	}()
 
-	web.Stop()
+	c.Close()
+
+	os.Exit(0)
 }
 
-func startWebView(addr string) {
+func getMainDataURI() string {
+	style, _ := resources.ReadFile("res/style.css")
+	styleDSL, _ := common.Files.ReadFile("res/dsl.css")
+
+	data := map[string]interface{}{
+		"Style":    template.CSS(style),
+		"StyleDSL": template.CSS(styleDSL),
+		"Script":   template.JS(jsgraphs.Script()),
+	}
+
+	buf := new(bytes.Buffer)
+	tpl := template.Must(template.ParseFS(resources, "res/main.html"))
+	tpl.Execute(buf, data)
+
+	return "data:text/html;charset=utf-8;base64," + base64.StdEncoding.EncodeToString(buf.Bytes())
+}
+
+func receive(stop chan bool) {
+	receiver := make(chan common.StateChange, 10)
+	c.RegisterReceiver(receiver)
+
+	defer func() {
+		c.UnregisterReceiver(receiver)
+	}()
+
+	for {
+		select {
+
+		case change := <-receiver:
+			updateState(common.GetStateMessage(change))
+
+		case <-stop:
+			return
+
+		}
+	}
+}
+
+func updateState(msg common.Message) {
+	w.Dispatch(func() {
+		w.Eval("if (window.updateState) updateState(" + string(msg.JSON()) + ")")
+	})
+}
+
+func showMessage(msg string) {
+	msgJSON, _ := json.Marshal(msg)
+
+	w.Dispatch(func() {
+		w.Eval("showMessage(" + string(msgJSON) + ")")
+	})
+}
+
+func initialized() {
+	change := c.State()
+	updateState(common.GetStateMessage(change))
+}
+
+func writeArchive(state common.StateChange) (filename string, err error) {
+	filenameBase := state.Time.Format("dsl_20060102_150405")
+	filename = filenameBase + ".zip"
+
+	f, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0666)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	err = common.WriteArchive(f, filenameBase, state)
+	return
+}
+
+func save() {
+	change := c.State()
+	if change.State != common.StateReady {
+		return
+	}
+
+	filename, err := writeArchive(change)
+
+	filename, _ = filepath.Abs(filename)
+
+	if err == nil {
+		showMessage(fmt.Sprintf("Saved to %s.", filename))
+	} else if errors.Is(err, fs.ErrExist) {
+		showMessage(fmt.Sprintf("File %s already exists.", filename))
+	} else if err != nil {
+		showMessage("Saving failed!")
+		fmt.Println("failed to save archive:", err)
+	}
+}
+
+func setPassword(data string) {
+	err := c.SetPassword(data)
+	if err != nil {
+		fmt.Println("unexpected call to setPassword")
+	}
+}
+
+func setPassphrase(data string) {
+	err := c.SetPassphrase(data)
+	if err != nil {
+		fmt.Println("unexpected call to setPassphrase")
+	}
+}
+
+func startWebView() {
 	w = webview.New(false)
 	defer w.Destroy()
 
 	w.SetTitle("xDSL stats")
+
+	w.SetSize(620, 300, webview.HintMin)
 	w.SetSize(620, 600, webview.HintNone)
 
-	w.Init(`document.addEventListener("DOMContentLoaded", function() { header = document.getElementsByTagName("header"); if (header.length) header[0].remove(); })`)
+	stop = make(chan bool, 1)
+	go receive(stop)
 
-	w.Navigate(addr)
+	w.Bind("goInitialized", initialized)
+	w.Bind("goSave", save)
+	w.Bind("goSetPassword", setPassword)
+	w.Bind("goSetPassphrase", setPassphrase)
+
+	script, _ := resources.ReadFile("res/script.js")
+	w.Init(string(script))
+
+	w.Navigate(getMainDataURI())
 
 	w.Run()
 }
 
 func stopWebView() {
 	if w != nil {
+		stop <- true
 		w.Terminate()
 	}
 }
---
+++ cmd/gui/res/script.js
@@ -0,0 +1,195 @@
+(function() {
+	"use strict";
+
+	const STATE_READY = "ready";
+	const STATE_PASSWORD = "password";
+	const STATE_PASSPHRASE = "passphrase";
+	const STATE_ERROR = "error";
+	const STATE_LOADING = "loading";
+
+	var state;
+
+	var eventSource;
+
+	var buttonSave;
+	var summary, graphs;
+	var graphBitsCanvas, graphSNRCanvas, graphQLNCanvas, graphHlogCanvas;
+	var graphBits, graphSNR, graphQLN, graphHlog;
+	var overlay, overlayPassword, overlayPassphrase, overlayError, overlayLoading;
+	var messages;
+	var fingerprint;
+
+	function updateState(newState, data) {
+		let oldState = state;
+
+		if (data !== undefined) {
+			switch (newState) {
+
+				case STATE_PASSPHRASE:
+					fingerprint.innerText = data;
+					break;
+
+				case STATE_READY:
+					var bins = DSLGraphs.decodeBins(data["bins"]);
+					var history = DSLGraphs.decodeBinsHistory(data["history"]);
+					summary.innerHTML = data["summary"];
+					graphBits.setData(bins);
+					graphSNR.setData(bins, history);
+					graphQLN.setData(bins);
+					graphHlog.setData(bins);
+					break;
+
+				case STATE_ERROR:
+					overlayError.innerText = data;
+
+			}
+		}
+
+		if (newState != oldState) {
+			state = newState;
+
+			buttonSave.classList.toggle("disabled", state != STATE_READY);
+
+			overlay.classList.toggle("visible", state != STATE_READY);
+			overlayPassword.classList.toggle("visible", state == STATE_PASSWORD);
+			overlayPassphrase.classList.toggle("visible", state == STATE_PASSPHRASE);
+			overlayError.classList.toggle("visible", state == STATE_ERROR);
+			overlayLoading.classList.toggle("visible", state == STATE_LOADING);
+		}
+	}
+
+	function showMessage(text) {
+		var msg = document.createElement("div");
+		msg.innerText = text;
+
+		messages.classList.add("visible");
+		messages.appendChild(msg);
+
+		window.setTimeout(function() {
+			cleanupMessage(msg)
+		}, 5000);
+	}
+
+	function cleanupMessage(msg) {
+		msg.remove();
+
+		if (messages.childElementCount == 0) {
+			messages.classList.remove("visible");
+		}
+	}
+
+	function sendForm(event) {
+		let form = event.target;
+		let formData = new FormData(form);
+		form.reset();
+
+		updateState("loading");
+
+		var target = form.dataset.target;
+		window[target](formData.get("data"));
+
+		event.preventDefault();
+	}
+
+	function initForms() {
+		let forms = document.getElementsByTagName("form");
+
+		for (let form of forms) {
+			form.addEventListener("submit", sendForm);
+		}
+	}
+
+	function getGraphParams(width, devicePixelRatio) {
+		var params = new DSLGraphs.GraphParams();
+
+		var width = width;
+		var height = 114;
+
+		params.width = Math.floor(width * devicePixelRatio);
+		params.height = Math.floor(height * devicePixelRatio);
+
+		params.scaleFactor = devicePixelRatio;
+
+		return params;
+	}
+
+	function setCanvasWidths(params) {
+		var width = (params.width / params.scaleFactor).toString() + "px";
+		graphBitsCanvas.style.width = width;
+		graphSNRCanvas.style.width = width;
+		graphQLNCanvas.style.width = width;
+		graphHlogCanvas.style.width = width;
+	}
+
+	function initGraphs() {
+		var lastDevicePixelRatio = window.devicePixelRatio;
+		var lastWidth = graphs.offsetWidth;
+
+		var params = getGraphParams(lastWidth, lastDevicePixelRatio);
+
+		graphBitsCanvas = document.getElementById("graph_bits");
+		graphSNRCanvas = document.getElementById("graph_snr");
+		graphQLNCanvas = document.getElementById("graph_qln");
+		graphHlogCanvas = document.getElementById("graph_hlog");
+
+		graphBits = new DSLGraphs.BitsGraph(graphBitsCanvas, params);
+		graphSNR = new DSLGraphs.SNRGraph(graphSNRCanvas, params);
+		graphQLN = new DSLGraphs.QLNGraph(graphQLNCanvas, params);
+		graphHlog = new DSLGraphs.HlogGraph(graphHlogCanvas, params);
+
+		setCanvasWidths(params);
+
+		window.addEventListener("resize", function() {
+			var devicePixelRatio = window.devicePixelRatio;
+			var width = graphs.offsetWidth;
+
+			if (devicePixelRatio != lastDevicePixelRatio || width != lastWidth) {
+				lastDevicePixelRatio = devicePixelRatio;
+				lastWidth = width;
+
+				var params = getGraphParams(width, devicePixelRatio);
+
+				graphBits.setParams(params);
+				graphSNR.setParams(params);
+				graphQLN.setParams(params);
+				graphHlog.setParams(params);
+
+				setCanvasWidths(params);
+			}
+		});
+	}
+
+	function loaded(event) {
+		buttonSave = document.getElementById("button-save");
+
+		summary = document.getElementById("summary");
+		graphs = document.getElementById("graphs");
+
+		overlay = document.getElementById("overlay");
+		overlayPassword = document.getElementById("overlay-password");
+		overlayPassphrase = document.getElementById("overlay-passphrase");
+		overlayError = document.getElementById("overlay-error");
+		overlayLoading = document.getElementById("overlay-loading");
+
+		messages = document.getElementById("messages");
+
+		fingerprint = document.getElementById("fingerprint");
+
+		updateState("loading");
+
+		window.updateState = function(data) {
+			updateState(data.state, data.data);
+		}
+
+		window.showMessage = function(text) {
+			showMessage(text);
+		}
+
+		initForms();
+		initGraphs();
+		goInitialized();
+	}
+
+	document.addEventListener("DOMContentLoaded", loaded);
+
+})();
