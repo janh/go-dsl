368b1246bc2c70fc82511671380d237ce2caa03f
Jan Hoffmann <jan@3e8.eu>
2023-04-18 13:50:13 +0200

    Add a string representation for errors history

--- models/history.go
+++ models/history.go
@@ -5,6 +5,9 @@
 package models
 
 import (
+	"fmt"
+	"io"
+	"strings"
 	"time"
 )
 
@@ -49,4 +52,44 @@ type ErrorsHistory struct {
 
 	DownstreamSESCount []IntValue
 	UpstreamSESCount   []IntValue
+}
+
+func (h ErrorsHistory) String() string {
+	var b strings.Builder
+
+	fmt.Fprintf(&b, "End time: %s\n", h.EndTime)
+	fmt.Fprintf(&b, "Period length: %s\n", h.PeriodLength)
+	fmt.Fprintf(&b, "Period count: %d\n", h.PeriodCount)
+	fmt.Fprintln(&b)
+
+	printErrorsHistoryList(&b, "Downstream rtx-tx", h.DownstreamRTXTXCount)
+	printErrorsHistoryList(&b, "Upstream rtx-tx", h.UpstreamRTXTXCount)
+
+	printErrorsHistoryList(&b, "Downstream rtx-c", h.DownstreamRTXCCount)
+	printErrorsHistoryList(&b, "Upstream rtx-c", h.UpstreamRTXCCount)
+
+	printErrorsHistoryList(&b, "Downstream rtx-uc", h.DownstreamRTXUCCount)
+	printErrorsHistoryList(&b, "Upstream rtx-uc", h.UpstreamRTXUCCount)
+
+	printErrorsHistoryList(&b, "Downstream FEC", h.DownstreamFECCount)
+	printErrorsHistoryList(&b, "Upstream FEC", h.UpstreamFECCount)
+
+	printErrorsHistoryList(&b, "Downstream CRC", h.DownstreamCRCCount)
+	printErrorsHistoryList(&b, "Upstream CRC", h.UpstreamCRCCount)
+
+	printErrorsHistoryList(&b, "Downstream ES", h.DownstreamESCount)
+	printErrorsHistoryList(&b, "Upstream ES", h.UpstreamESCount)
+
+	printErrorsHistoryList(&b, "Downstream SES", h.DownstreamSESCount)
+	printErrorsHistoryList(&b, "Upstream SES", h.UpstreamSESCount)
+
+	return b.String()
+}
+
+func printErrorsHistoryList(w io.Writer, label string, data []IntValue) {
+	fmt.Fprintf(w, "%s:", label)
+	for _, val := range data {
+		fmt.Fprintf(w, " %s", val)
+	}
+	fmt.Fprintf(w, "\n\n")
 }
